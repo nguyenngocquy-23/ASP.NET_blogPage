0fdc02da38189a1bf575cd050dc1be1159430b4f
kien192 <trungkien19203@gmail.com>
2024-08-03 18:21:16 +0700

    git feat(comment) add method Get and post comment for blog

---
+++ apiServer/DTO/CommentDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace apiServer.DTO
+{
+    public class CommentDTO
+    {
+        public string UserId { get; set; }
+        public string Content { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
