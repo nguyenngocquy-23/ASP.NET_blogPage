fc1a748e55edf9054e28800ac7922a2025043338
kien192 <trungkien19203@gmail.com>
2024-08-21 09:17:54 +0700

    upload new code for comment manage

---
+++ news/src/components/listAuthBlog/ListAuthBlog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+ListAuthBlog.propTypes = {
+
+
+};
+
+function ListAuthBlog(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default ListAuthBlog;
